c9ce7774fca8f4538dcdc13160ba5f1e8616369c
Olof Kindgren <olof.kindgren@gmail.com>
2015-02-27 22:22:42 +0100

    i2c_master_byte_ctrl: Fix syntax error for my_addr

--- rtl/verilog/i2c_master_byte_ctrl.v
+++ rtl/verilog/i2c_master_byte_ctrl.v
@@ -85,7 +85,7 @@ module i2c_master_byte_ctrl
 	// inputs & outputs
 	//
 	input clk;     // master clock
-    input my_addr; // Slave address input
+    input [6:0] my_addr; // Slave address input
 	input rst;     // synchronous active high reset
 	input nReset;  // asynchronous active low reset
 	input ena;     // core enable signal
